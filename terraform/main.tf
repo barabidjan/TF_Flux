792dcd0881ecd60e039d5ec75af0392fedb06864
barabidjan <ydibka@gmail.com>
2025-09-15 21:57:35 +0300

    config files update

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "gcs" {
+    bucket = "inv-secret"
+    prefix = "terraform/state"
+  }
+}
+
+
+
+
+module "gke_cluster" {
+  source         = "github.com/barabidjan/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 2
+}
