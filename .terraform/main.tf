5b8777697fa1612aea300ffc025cf47a68e28caa
Igor Artemchuk <ydibka@gmail.com>
2025-09-16 13:57:12 +0000

    add files and configs

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -6,11 +6,33 @@ terraform {
 }
 
 
+module "github_repository" {
+  source                   = "github.com/barabidjan/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux"
+}
 
+module "tls_private_key" {
+  source    = "github.com/barabidjan/tf-hashicorp-tls-keys"
+  algorithm = "RSA"
+}
 
-module "gke_cluster" {
-  source         = "github.com/barabidjan/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+# Source: "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
+module "kind_cluster" {
+  source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
+}
+
+# # Source: "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
+module "flux_bootstrap" {
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_host       = module.kind_cluster.endpoint
+  config_client_key = module.kind_cluster.client_key
+  config_ca         = module.kind_cluster.ca
+  config_crt        = module.kind_cluster.crt
+  github_token      = var.GITHUB_TOKEN
 }
--- .terraform/variables.tf
+++ .terraform/variables.tf
@@ -9,7 +9,24 @@ variable "GOOGLE_REGION" {
   description = "GCP region to use"
 }
 
-variable "GKE_NUM_NODES" {
-  type        = number
-  description = "Number of nodes"
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "GitHub owner repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
 }
